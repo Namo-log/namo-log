3ca81b75520e13b5ca3f6d511ad2fa9fad6d1288
Nahyun Kim <101113025+nahy-512@users.noreply.github.com>
2024-04-26 03:03:28 +0900

    빌드 이슈 수정
    유효하지 않은 문자

--- site.config.js
+++ site.config.js
@@ -36,7 +36,7 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: true,
+    enable: true,
     config: {
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
