0814b064eccb1a29a07eab220b72ce87c6ea079d
jodan <wdd789q@naver.com>
2024-06-25 14:38:18 +0900

    fix : URL 공유시 이미지 2개 보이는 버그 2차 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
-import { CONFIG } from "site.config"
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CONFIG } from "site.config";
 
 class MyDocument extends Document {
   render() {
@@ -19,37 +19,35 @@ class MyDocument extends Document {
             href="/feed"
           ></link>
 
-          {/* Open Graph meta tags for social media sharing */}
-          <meta property="og:title" content={CONFIG.blog.title} />
-          <meta property="og:description" content={CONFIG.blog.description} />
-          <meta property="og:image" content="https://namo-log.vercel.app/images/thumbnail.jpg" />
-          <meta property="og:url" content="https://namo-log.vercel.app/" />
-          <meta property="og:type" content="website" />
-
-          {/* Twitter Card meta tags for Twitter sharing */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={CONFIG.blog.title} />
-          <meta name="twitter:description" content={CONFIG.blog.description} />
-          <meta name="twitter:image" content="https://namo-log.vercel.app/images/thumbnail.jpg" />
-          <meta name="twitter:url" content="https://namo-log.vercel.app/" />
-
-          {/* google search console */}
-          {CONFIG.googleSearchConsole.enable === true && (
+          {/* 홈 페이지 Open Graph 메타 태그 */}
+          {this.props.__NEXT_DATA__.page === "/" && (
             <>
-              <meta
-                name="google-site-verification"
-                content={CONFIG.googleSearchConsole.config.siteVerification}
-              />
+              <meta property="og:title" content={CONFIG.blog.title} />
+              <meta property="og:description" content={CONFIG.blog.description} />
+              <meta property="og:image" content="https://namo-log.vercel.app/images/thumbnail.jpg" />
+              <meta property="og:url" content="https://namo-log.vercel.app/" />
+              <meta property="og:type" content="website" />
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:title" content={CONFIG.blog.title} />
+              <meta name="twitter:description" content={CONFIG.blog.description} />
+              <meta name="twitter:image" content="https://namo-log.vercel.app/images/thumbnail.jpg" />
+              <meta name="twitter:url" content="https://namo-log.vercel.app/" />
             </>
           )}
-          {/* naver search advisor */}
+
+          {/* Google Search Console */}
+          {CONFIG.googleSearchConsole.enable === true && (
+            <meta
+              name="google-site-verification"
+              content={CONFIG.googleSearchConsole.config.siteVerification}
+            />
+          )}
+          {/* Naver Search Advisor */}
           {CONFIG.naverSearchAdvisor.enable === true && (
-            <>
-              <meta
-                name="naver-site-verification"
-                content={CONFIG.naverSearchAdvisor.config.siteVerification}
-              />
-            </>
+            <meta
+              name="naver-site-verification"
+              content={CONFIG.naverSearchAdvisor.config.siteVerification}
+            />
           )}
         </Head>
         <body>
@@ -57,8 +55,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
